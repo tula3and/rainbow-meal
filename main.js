0c323bfe91dc415bd374b762649302df5ca11ddd
unknown <tula3and@gmail.com>
2020-11-21 02:01:16 +0900

    Add sorting function outlines

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ const toogleBtn = document.querySelector('.navbar__toogle');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
+const price = document.querySelector('#price');
+const upvote = document.querySelector('#upvote');
+const place = document.querySelector('#place');
+
 toogleBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
+});
+
+price.addEventListener('click', function filter() {
+
+});
+
+upvote.addEventListener('click', function filter() {
+
+});
+
+place.addEventListener('click', function filter() {
+
 });
